d5c7819e80f98d35d657d856920a8826ecf84d2b
tanay-hv <tanay.t@hyperverge.co>
2024-10-30 19:45:21 +0530

    Added feature extraction service

--- src/routes/recogniseUser.py
+++ src/routes/recogniseUser.py
@@ -1,15 +1,22 @@
 from fastapi import APIRouter, UploadFile, Depends, File
 from services.faceDetectionService import FaceDetectionService
+from services.featureExtractionService import FeatureExtractionService
 
 router = APIRouter()
 
 @router.post("/recogniseUser")
 async def recogniseUser(
     image : UploadFile = File(...),
-    faceDetectionService : FaceDetectionService = Depends()
+    faceDetectionService : FaceDetectionService = Depends(),
+    featureExtractionService : FeatureExtractionService = Depends()
 ):
     imageContent = await image.read()
 
     facePixels, confidence = await faceDetectionService.detectFace(imageContent)
 
-    return {"success" : confidence}
+    vectors = await featureExtractionService.extractFeatures(facePixels=facePixels)
+
+    return  {
+                "confidence" : confidence,
+                "vectors" : vectors
+            }
---
+++ src/services/featureExtractionService.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications import InceptionResNetV2
+from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from PIL import Image
+
+class FeatureExtractionService:
+    def __init__(self):
+        self.model = InceptionResNetV2(
+            weights='imagenet',
+            include_top=False,
+            pooling='avg'
+        )
+    
+    def preprocessFace(self, facePixels: np.ndarray) -> np.ndarray:
+        if facePixels.shape[2] == 4:
+            facePixels = facePixels[:,:,:3]
+
+        image = Image.fromarray(facePixels)
+        image = image.resize((250, 250))
+
+        facePixels = img_to_array(image)
+        facePixels = preprocess_input(facePixels)
+
+        return facePixels
+    
+    async def extractFeatures(self, facePixels : np.ndarray) -> np.ndarray :
+        processedFace = self.preprocessFace(facePixels=facePixels)
+        expanded = np.expand_dims(processedFace, axis=0)
+        features = self.model.predict(expanded, verbose = 0)
+        return features[0].tolist()
